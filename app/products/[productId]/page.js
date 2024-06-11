aa03676132eece261ad07377e98dc0dfae353122
devarrasgroup <developer@arrasgroupspa.com>
2024-06-11 17:04:03 +0200

    initial commit

---
+++ app/products/[productId]/page.js
@@ -0,0 +1,5 @@
+export default function Product({params}) {
+    return (
+        <div>Product ID: {params.productId} detail</div>
+    );
+}
---
+++ app/products/page.js
@@ -0,0 +1,9 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>
+                Hello products
+            </h1>
+        </div>
+    )
+}
